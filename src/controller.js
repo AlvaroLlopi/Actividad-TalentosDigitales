d83f3625741d8f29bbf10b88fde175e70293297e
AlvaroLlopi <110068555+AlvaroLlopi@users.noreply.github.com>
2023-10-23 12:00:24 -0300

    delete con try catch

--- src/controller.js
+++ src/controller.js
@@ -24,13 +24,14 @@ class LibrosController {
     async add(req, res) {
         try {
             const libro = req.body;
-            if (!libro.nombre || !libro.autor || !libro.categoria || !libro.añopublicacion || !libro.ISBN) {
-                throw new Error("Completar campos faltantes");
+            if (libro.nombre && libro.autor && libro.categoria && libro.añopublicacion && libro.ISBN) {
+                const [result] = await pool.query(`INSERT INTO libros(nombre,autor,categoria,añopublicacion,ISBN) VALUES (?,?,?,?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.añopublicacion, libro.ISBN]);
+            res.json({ "id insertado": result.insertId });    
             }
-            const [result] = await pool.query(`INSERT INTO libros(nombre,autor,categoria,añopublicacion,ISBN) VALUES (?,?,?,?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.añopublicacion, libro.ISBN]);
-            res.json({ "id insertado": result.insertId });
+            throw new Error("Completar campos faltantes");
         } catch (error) {
-            res.status(400).json({ error: error.message });
+            console.error("Error al añadir el libro:", error);
+            res.status(500).json({ "Mensaje": "Error en el servidor" });
         }
     }
 
@@ -39,6 +40,21 @@ class LibrosController {
         const [result] = await pool.query(`DELETE FROM libros WHERE ISBN=(?)`, [libro.ISBN]);
         res.json({ "Registro Eliminado": result.affectedRows });
     }
+
+    async delete(req, res) {
+        try {
+          const libro = req.body;
+          if (libro.ISBN) {
+            const [result] = await pool.query(`DELETE FROM libros WHERE ISBN=(?)`, [libro.ISBN]); 
+          res.json({ "Registro Eliminado": result.affectedRows });
+          }
+          res.status(404).json({ "Mensaje": "No se encontró el libro con el ISBN proporcionado" });
+        } catch (error) {
+            console.error("Error al buscar el libro por ISBN:", error);
+            res.status(500).json({ "Mensaje": "Error en el servidor" });
+        }
+      }
+      
 
     async update(req, res) {
         const libro = req.body;
